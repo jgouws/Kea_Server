50bc21cfc2f47d4ca39c24db69d6bcc480a09619
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-08-02 15:14:14 +1200

    init

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+const databaseName = 'express_tdd';
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${databaseName}`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
+  test: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${databaseName}_testing`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  }
+};
