62e9c5f9b1dd67cc71de5b7063366b1cd6382268
lawnthom <lawnthom@vuw.ac.nz>
2017-08-11 16:54:41 +1200

    Allows images, shows error page when non whitelisted file type uploaded instead.

--- src/server/routes/uploadedimage.js
+++ src/server/routes/uploadedimage.js
@@ -11,21 +11,35 @@ router.post('/', function (req, res, next) {
 
   var form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files) {
-    var oldpath = files.filetoupload.path;
-    console.log('saved ' + files.filetoupload.path);
-    //var newpath = '/Users/nigelmunro/desktop/nodefileupload' + files.filetoupload.name;
-    var newpath = './pictures/' + files.filetoupload.name;
-
-    fs.rename(oldpath, newpath, function (err) {
-      if (err) throw err;
-      res.write('File uploaded and moved!');
+    // check file to make sure it's a image file, only uses file extension
+    var filetype = files.filetoupload.type;
+    var allowedTypes = [
+      'image/png',
+      'image/jpeg',
+      'image/gif'
+    ]
+    var found = allowedTypes.indexOf(filetype);
+    if(found < 0){
+      // invalid file
+      res.write('Invlaid file type');
       res.end();
+    }
+    else{
+      var oldpath = files.filetoupload.path;
+      console.log('saved ' + files.filetoupload.path);
+      //var newpath = '/Users/nigelmunro/desktop/nodefileupload' + files.filetoupload.name;
+      var newpath = './pictures/' + files.filetoupload.name;
 
-      //res.render('uploadedimage', renderObject);
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
 
-    });
+       //res.render('uploadedimage', renderObject);
+
+      });
+    }
   });
-
 });
 
 module.exports = router;
