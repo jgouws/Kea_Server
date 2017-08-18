c347ad37940c512be8b19c9902874903603c4bef
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-08-18 14:20:26 +1200

    removed /auth presence

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -8,14 +8,13 @@
     const routes = require('../routes/index');
     const usersRoutes = require('../routes/users');
     const userRoutes = require('../routes/user');
-    const authRoutes = require('../routes/auth');
+    // const authRoutes = require('../routes/auth');
     const uploadimageRoutes = require('../routes/uploadimage');
     const uploadedimageRoutes = require('../routes/uploadedimage');
 
     // *** register routes *** //
     app.use('/', routes);
     app.use('/api/v1/users', usersRoutes);
-    app.use('/auth', authRoutes);
     app.use('/', userRoutes);
     app.use('/uploadimage', uploadimageRoutes);
     app.use('/uploadedimage', uploadedimageRoutes);
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const indexController = require('../controllers/index');
 
+const authHelpers = require('../auth/_helpers');
+const passport = require('../auth/local');
+
 router.get('/', function (req, res, next) {
   const renderObject = {};
   renderObject.title = 'Kaka Go';
@@ -38,5 +41,34 @@ router.get('/login', function (req, res, next) {
   renderObject.title = 'Please Log-in to continue';
   res.render('login', renderObject);
 });
+
+router.get('/register', (req, res, next)  => {
+  res.render('register', {title: 'Register'});
+});
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.log(err.stack);
+      handleResponse(res, 500, 'error');
+    }
+    if (!user) { handleResponse(res, 404, 'User not found'); }
+    if (user) {
+      req.logIn(user, function (err) {
+        if (err) { handleResponse(res, 500, 'error'); }
+        handleResponse(res, 200, 'success');
+      });
+    }
+  })(req, res, next);
+});
+
+router.get('/logout', authHelpers.loginRequired, (req, res, next) => {
+  req.logout();
+  handleResponse(res, 200, 'success');
+});
+
+function handleResponse(res, code, statusMsg) {
+  res.status(code).json({status: statusMsg});
+}
 
 module.exports = router;
