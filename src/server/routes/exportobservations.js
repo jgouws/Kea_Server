3a0e558e039bdf9094e5187f3c362b48e137e913
lawnthom <lawnthom@vuw.ac.nz>
2017-09-15 11:17:37 +1200

    Exports all database contents now

--- src/server/routes/exportobservations.js
+++ src/server/routes/exportobservations.js
@@ -9,7 +9,10 @@ router.get('/', function (req, res, next) {
 
   var query = knex.select('*').from('observations').then(function(result) {
       for (var i = 0 ; i < result.length; i++) {
-        csv += '' + result[i].id + ', ' + result[i].species + ', ' + result[i].description + ', ' + result[i].image_url  + '\n';
+        csv += '' + result[i].id + ', ' + result[i].user_id + ', /uploaded/' + result[i].image_url;
+        csv += ', ' + result[i].species + ', ' + result[i].description + ', ' + result[i].approved;
+        csv += ', ' + result[i].latitude + ', ' + result[i].longitude + ', ' + result[i].description;
+        csv += '\n';
         //renderObject.rows.push(newRow);
       }
 
