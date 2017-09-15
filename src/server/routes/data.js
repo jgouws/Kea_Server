80cef95c00a59eed084c64aa12161721edb2e954
Benjamin Mccarthy <mccartbenj1@kipp-cafe.ecs.vuw.ac.nz>
2017-09-15 13:36:16 +1200

    Changed data.pug to use a new javascript function

---
+++ src/server/routes/data.js
@@ -0,0 +1,27 @@
+
+function parseQuery(String queryRecieved, Boolean[] attributeSelection){
+  var data = [[][]];
+
+  var rowLength;
+  var numberRows;
+
+  //Split according to line structure
+  //assign rowLength and numberRows
+  //load strings into data
+
+  for(var y = 0; y < data.length; y++){
+    for(var x = 0; x < data[0].length; x++){
+      String entry = data[y][x];
+    }
+  }
+
+  return data;
+}
+
+//Get bool array
+ //
+
+ function tempClick(){
+
+   console.console.log("onclick working");
+ }
