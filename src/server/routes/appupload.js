b4c7e2594917ac89a568f93251da6b293c5c4049
lawnthom <lawnthom@vuw.ac.nz>
2017-09-22 12:04:05 +1200

    Page requires post form for app

--- src/server/routes/appupload.js
+++ src/server/routes/appupload.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../../../src/server/db/knex');
 
-
-router.post('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
 
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
         
     });
+    
 });
 
 module.exports = router;
