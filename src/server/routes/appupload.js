49bb4409c0b0d19307fc6c0b40f1e30d3080b703
lawnthom <lawnthom@vuw.ac.nz>
2017-09-22 12:12:29 +1200

    Added debugging logs

--- src/server/routes/appupload.js
+++ src/server/routes/appupload.js
@@ -6,7 +6,10 @@ router.get('/', function (req, res, next) {
 
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
-        
+        console.log('--------- files  --------')
+        console.log(files);
+        console.log('--------- feilds --------')
+        console.log(fields);
     });
     
 
