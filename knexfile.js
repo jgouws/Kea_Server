20a7fd6bbcc817ec05f2e0932c97077198d1a0af
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-09-29 09:16:57 +1300

    connecting eb to rds database. adds options in nodecommand.config, new environment details in knexfile.js. I also have a credentials.txt to be ignored in .gitignore

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,22 @@
 const databaseName = 'kakago';
 
 module.exports = {
+  aws_env: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
   development: {
     client: 'postgresql',
     connection: `postgres://localhost:5432/${databaseName}`,
