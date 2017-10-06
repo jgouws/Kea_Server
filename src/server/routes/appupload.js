284b55c1ee61f49e65ba09eb8086408b27d63da5
Michael Kane <kanemich2@ecs.vuw.ac.nz>
2017-10-06 17:23:47 +1300

    Comment out automated testing temporarily

--- src/server/routes/appupload.js
+++ src/server/routes/appupload.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const formidable = require('formidable');
 const knex = require('../../../src/server/db/knex');
 
-router.get('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
+
+  bodydata = req.body;
+  app_description = bodydata.description;
+  console.log('description=' + app_description);
+
 
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files) {
@@ -11,7 +16,39 @@ router.get('/', function (req, res, next) {
     console.log(files);
     console.log('--------- feilds --------');
     console.log(fields);
+
+
+    knex('uid').select('value').then(function(result) {
+      // Storage location for saved images
+      image_folder = '/uploaded/';
+      uid = result[0].value;
+      // check if the right value
+      console.log('uid=' + uid);
+
+      // Insert observation text with the rest default values
+      knex('observations').insert({
+        user_id: 1,
+        image_url: uid,
+        species: 'spooky skeleton via IOS',
+        description: app_description,
+        approved: false,
+        latitude: '41.2865',
+        longitude: '174.7762',
+        created_at: new Date()
+
+      }).then(function(result) {
+        console.log('inserted value');
+        // Update the UID value in the database
+        knex('uid').where('id', '=', '1').update({
+          value: uid + 1
+        }).then();
+      });
+
+      res.write('Observation successfully submitted');
+      res.end();
+    });
+
+
   });
 });
-
 module.exports = router;
